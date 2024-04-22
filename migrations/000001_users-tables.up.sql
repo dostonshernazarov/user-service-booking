4a87b942b3102339d1a0e06073e011fcbb3b41c8
Awez1459 <avazbekbekmurodov1459@gmail.com>
2024-04-22 10:57:07 +0500

    Migrations added

---
+++ migrations/000001_users-tables.up.sql
@@ -0,0 +1,17 @@
+CREATE TABLE IF NOT EXISTS users (
+    id UUID NOT NULL,
+    full_name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    date_of_birth VARCHAR(255),
+    profile_img VARCHAR(255),
+    card VARCHAR(255),
+    gender VARCHAR(255),
+    phone_number VARCHAR(255),
+    role VARCHAR(255),
+    establishment_id UUID,
+    created_at TIMESTAMP NOT NULL,
+    updated_at TIMESTAMP,
+    deleted_at TIMESTAMP,
+    PRIMARY KEY (id)
+);
