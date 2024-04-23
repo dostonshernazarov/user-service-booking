b83c0227d57c370b3cfe3f34fb424ba104097bce
Awez1459 <avazbekbekmurodov1459@gmail.com>
2024-04-23 16:40:58 +0500

    migrations without FK

--- migrations/000001_users-tables.up.sql
+++ migrations/000001_users-tables.up.sql
@@ -29,11 +29,7 @@ CREATE TABLE IF NOT EXISTS userHotelBooking (
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (room_id) REFERENCES establishments(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+    PRIMARY KEY (id)
 );
 
 CREATE TABLE IF NOT EXISTS userRestaurantBooking (
@@ -41,12 +37,11 @@ CREATE TABLE IF NOT EXISTS userRestaurantBooking (
     user_id UUID NOT NULL,
     table_id UUID NOT NULL,
     will_arrive TIMESTAMP,
+    number_of_people INTEGER,
+    is_canceled BOOLEAN DEFAULT FALSE,
+    reason VARCHAR(255) IF is_canceled IS TRUE,
     created_at TIMESTAMP NOT NULL,
     updated_at TIMESTAMP,
     deleted_at TIMESTAMP,
-    PRIMARY KEY (id),
-    FOREIGN KEY (user_id) REFERENCES users(id),
-    FOREIGN KEY (table_id) REFERENCES establishments(id)
-    ON DELETE CASCADE
-    ON UPDATE CASCADE
+    PRIMARY KEY (id)
 );
